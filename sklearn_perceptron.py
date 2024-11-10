43c94284e47361a9f57f366912b99dc7bb0cb678
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-11-10 14:03:42 -0800

    Update sklearn_perceptron.py

--- sklearn_perceptron.py
+++ sklearn_perceptron.py
@@ -1,5 +1,8 @@
 from sklearn import datasaets 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import numpy as np 
+import pandas as pd 
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
