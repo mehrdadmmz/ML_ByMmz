8e0dcf8603f8965e7d84424126cccb3b0fc2b64b
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-11-14 15:20:45 -0800

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -58,7 +58,7 @@ class LogisticRegressionGD:
             self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
             self.b_ += self.eta * 2.0 * errors.mean()
             loss = ((-y.dot(np.log(output))) - ((1-y).dot(np.log(1-output)))) / X.shape[0]
-            self.losses_.appand(loss)
+            self.losses_.append(loss)
             
         return self
             
@@ -75,3 +75,40 @@ class LogisticRegressionGD:
     def predict(self, X): 
         """Return class label after unit step"""
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)    
+    
+    
+if __name__ == "__main__": 
+        """
+        when we fit a logistic reg model, we have to keep in mind that it only works 
+        for binary classification tasks. So, we only consider setosa, and versicolor
+        (class 0, 1)
+        """
+        iris = datasets.load_iris()
+        X = iris.data[:100, [2, 3]]
+        y = iris.target[:100]
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                            test_size=0.3, 
+                                                            random_state=1, 
+                                                            stratify=y, )
+        sc = StandardScaler()
+        X_train_std = sc.fit_transform(X_train)
+        X_test_std = sc.fit_transform(X_test)
+        
+        X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+        y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+        
+        lrgd = LogisticRegressionGD(eta=0.3, 
+                                    n_iter=1000, 
+                                    random_state=1)
+        
+        lrgd.fit(X_train_01_subset, y_train_01_subset)
+        plot_decision_regions(X = X_train_01_subset, 
+                              y = y_train_01_subset, 
+                              classifier=lrgd)
+        
+        plt.xlabel("Petal length [standardized]")
+        plt.ylabel("Peal width [standardized]")
+        plt.legend(loc="best")
+        plt.tight_layout()
+        plt.show()
