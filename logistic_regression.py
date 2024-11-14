1dbe0872944fdb535016ca3b7f5dc85b140f85cc
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-11-14 15:28:11 -0800

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,10 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
 
 
 class LogisticRegressionGD: 
