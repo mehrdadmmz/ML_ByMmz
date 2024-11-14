1efe3d4862bcad67f6137cde30ea93e7f9a82e59
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-11-14 15:24:33 -0800

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,9 @@
 import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 class LogisticRegressionGD: 
     """
