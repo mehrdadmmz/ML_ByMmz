3371cdc9b5a338bcaf67dfc5f704731c87b657b6
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-17 17:33:43 -0800

    Update sequential_backward_selection.py

--- Data preprocessing/sequential_backward_selection.py
+++ Data preprocessing/sequential_backward_selection.py
@@ -133,6 +133,20 @@ if __name__ == "__main__":
     # below we can see that as we decreased the number of features from 13, we increased the accuracy
     # and that is due to the curse of dimensionality in KNN
     # 100 percent acc on k = {3, 7, 8, 9, 10, 11, 12}
+    # sbs.subsets_
+    # [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+    # (0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12),
+    # (0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11),
+    # (0, 1, 2, 3, 4, 5, 6, 7, 9, 11),
+    # (0, 1, 2, 3, 4, 5, 7, 9, 11),
+    # (0, 1, 2, 3, 5, 7, 9, 11),
+    # (0, 1, 2, 3, 5, 7, 11),
+    # (0, 1, 2, 3, 5, 11),
+    # (0, 1, 2, 3, 11),
+    # (0, 1, 2, 11),
+    # (0, 1, 11),
+    # (0, 11),
+    # (0,)]
     k_feat = [len(k) for k in sbs.subsets_]
     plt.plot(k_feat, sbs.scores_, marker='o')
     plt.ylabel("Accuracy")
