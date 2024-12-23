c546be017b6f309a0417fba56bc59ca92778da87
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-23 04:17:02 -0800

    Update LDA_sklearn.py

--- 8 - Dimensionality Reduction Feature Extraction/LDA_sklearn.py
+++ 8 - Dimensionality Reduction Feature Extraction/LDA_sklearn.py
@@ -28,7 +28,7 @@ transformation matrix, W; the eigenvectors are the columns of this matrix.
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     
     # setup marker generator and color map
-    markers = ('o', 's', '^', 'v', '<')
+    markers = ('o', 's', '^', 'D', 'p')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
     
