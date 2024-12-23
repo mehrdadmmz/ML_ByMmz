4eec4b7da4c6983ef2760c2c566d9d5eead4de9c
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-23 03:46:30 -0800

    Update LDA.py

--- 8 - Dimensionality Reduction Feature Extraction/LDA.py
+++ 8 - Dimensionality Reduction Feature Extraction/LDA.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
-from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
