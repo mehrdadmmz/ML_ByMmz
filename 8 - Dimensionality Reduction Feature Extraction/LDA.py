65f5754dc48fd3b9571cdfb9f6d229a467784d52
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-23 03:03:00 -0800

    Update LDA.py

--- 8 - Dimensionality Reduction Feature Extraction/LDA.py
+++ 8 - Dimensionality Reduction Feature Extraction/LDA.py
@@ -54,8 +54,46 @@ X_test_std = stdscl.transform(X_test)
 # 
 # Set printing options
 # These options determine the way floating point numbers, arrays and other NumPy objects are displayed.
+# shape of each mean vector will be one dim vector of 13 elements, each of which is the mean of each  
+# feature that belongs to class i in {1, 2, 3} here. 
+# Like for class 1, we filter everything that is for class 1 first, then we will find the mean of each feature
+# its shape will be (13, )
 np.set_printoptions(precision=4)
 mean_vecs = []
 for label in range(1, 4): 
     mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
     print(f"MV {label}: {mean_vecs[label - 1]}\n")
+    
+d = 13 # number of features
+mean_overall = np.mean(X_train_std, axis=0).reshape(d, 1)
+
+# Within-class scatter matrix S_W
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T)
+    S_W += class_scatter
+    
+print('Within-class scatter matrix: '
+      f'{S_W.shape[0]}x{S_W.shape[1]}')
+
+# Between-class scatter matrix S_B
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train_std[y_train == i + 1, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1) # make column vector
+    S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+
+print('Between-class scatter matrix: '
+      f'{S_B.shape[0]}x{S_B.shape[1]}')
+
+# eigenvlaues, eigenvectors, and eigenpairs of the (SW)^-1(SB)
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+
+# Sorting them (decreasing from largest to the smallest) based on the eigen val and its associated eigen vec
+eigen_pairs = sorted(eigen_pairs, key=lambda k : k[0], reverse=True)
+print("\nEigenvalues in descending order: \n")
+
+# In LDA, the number of linear discriminants is at most c-1 with c being the number of classes
+for eigen_val in eigen_pairs: 
+    print(eigen_val[0])
