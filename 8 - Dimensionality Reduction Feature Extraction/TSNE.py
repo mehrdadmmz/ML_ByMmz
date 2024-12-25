d48c09b71c9c62ebaf7c24864c706210ba3a9725
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-24 23:49:00 -0800

    Update TSNE.py

--- 8 - Dimensionality Reduction Feature Extraction/TSNE.py
+++ 8 - Dimensionality Reduction Feature Extraction/TSNE.py
@@ -1,1 +1,53 @@
-"""t-distributed stochastic neighbour embedding (t-SNE)"""
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+from sklearn.datasets import load_digits
+from sklearn.manifold import TSNE
+
+""" t-distributed stochastic neighbour embedding (t-SNE): 
+non-linear dimensionality reduction technique. TSNE is best for Visualizing high-dimensional data in 2D or 3D.
+
+The development and application of nonlinear dimensionality reduction techniques is also often
+referred to as manifold learning, where a manifold refers to a lower dimensional topological space
+embedded in a high-dimensional space.
+
+t-SNE learns to embed data points into a lower-dimensional space
+such that the pairwise distances in the original space are preserved. 
+
+Since it projects the points directly (unlike PCA, it does not involve a projection matrix), 
+we cannot apply t-SNE to new data points.
+"""
+
+
+digits = load_digits()
+
+fig, ax = plt.subplots(1, 4)
+for i in range(4):
+    ax[i].imshow(digits.images[i], cmap='Greys') # Display data as an image
+
+plt.show() # shows the first 4 images
+
+X_digits = digits.data # (1797, 64) pixel values of each ex ranging from 0(completely black) to 16(completely white)
+y_digits = digits.target # the actual numbers 0-9
+
+# init='pca', which initializes the t-SNE embedding using PCA
+tsne = TSNE(n_components=2, init='pca', random_state=123)
+
+# projecting 64-dim dataset onto 2-dim space
+X_digits_tsne = tsne.fit_transform(X_digits)
+
+def plot_projection(x, colors): 
+    
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    
+    for i in range(10): 
+        plt.scatter(x[colors == i, 0], x[colors == i, 1])
+    
+    for i in range(10):
+        xtext, ytext = np.median(x[colors == i, :], axis=0)
+        txt = ax.text(xtext, ytext, str(i), fontsize=24)
+        txt.set_path_effects([PathEffects.Stroke(linewidth=5, foreground='w'), 
+                              PathEffects.Normal()])
+        
+plot_projection(X_digits_tsne, y_digits)
+plt.show()
