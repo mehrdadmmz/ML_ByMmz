02a3e5b251765a6ab42e5c78eab2032e575d58d4
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-26 01:57:07 -0800

    Update cross_validation.py

--- 9 - Model Evaluation and Hyperparameter Tuning/cross_validation.py
+++ 9 - Model Evaluation and Hyperparameter Tuning/cross_validation.py
@@ -59,12 +59,11 @@ print(f"Test accuracy: {test_acc:.3f}")
 
 # Performing stratified k-fold cross-validation 
 kfold = StratifiedKFold(n_splits=10).split(X_train, y_train)
-scores = []
+scores = cross_val_score(estimator=pipe_lr, 
+                         X=X_train, 
+                         y=y_train, 
+                         cv=10, 
+                         n_jobs=1)
 
-for k, (train, test) in enumerate(kfold): 
-    pipe_lr.fit(X_train[train], y_train[train])
-    score = pipe_lr.score(X_train[test], y_train[test])
-    scores.append(score)
-    print(f'Fold: {k+1:02d}, '
-          f'Class distr.: {np.bincount(y_train[train])}, '
-          f'Acc.: {score:.3f}')
+print(f'CV accuracy scores" {scores}')
+print(f"\nCV accuracy: {np.mean(scores):.3f} +/- {np.std(scores):.3f}")
