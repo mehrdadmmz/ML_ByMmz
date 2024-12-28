7c297f12f77dfda0b72d0b25181d403eace4dd16
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-12-28 02:23:11 -0800

    Update learning_curves.py

--- 9 - Model Evaluation and Hyperparameter Tuning/learning_curves.py
+++ 9 - Model Evaluation and Hyperparameter Tuning/learning_curves.py
@@ -1,6 +1,9 @@
 """
 Here, we will discuss how we can use learning curves to diagnose whether a learning algorithm
 has a problem with overfitting (high variance) or underfitting (high bias).
+
+Learning curves:   plotting training and test accuracies as a func of sameple size
+Validation curves: we vary the values of the model parameters like parameter C in logistic reg 
 """
 
 import pandas as pd 
