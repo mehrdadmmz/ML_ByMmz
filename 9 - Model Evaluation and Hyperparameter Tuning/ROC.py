41b26c3047f56268cf48cccb8ba61f7646b1908f
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2025-01-14 13:03:39 -0800

    Create ROC.py

---
+++ 9 - Model Evaluation and Hyperparameter Tuning/ROC.py
@@ -0,0 +1,53 @@
+# receiver operating characteristic
+
+# ROC
+
+from sklearn.metrics import roc_curve, auc
+from numpy import interp
+pipe_lr = make_pipeline(
+StandardScaler(),
+PCA(n_components=2),
+... LogisticRegression(penalty='l2', random_state=1,
+... solver='lbfgs', C=100.0)
+... )
+>>> X_train2 = X_train[:, [4, 14]]
+>>> cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
+>>> fig = plt.figure(figsize=(7, 5))
+>>> mean_tpr = 0.0
+>>> mean_fpr = np.linspace(0, 1, 100)
+>>> all_tpr = []
+>>> for i, (train, test) in enumerate(cv):
+... probas = pipe_lr.fit(
+... X_train2[train],
+... y_train[train]
+... ).predict_proba(X_train2[test])
+... fpr, tpr, thresholds = roc_curve(y_train[test],
+... probas[:, 1],
+... pos_label=1)
+... mean_tpr += interp(mean_fpr, fpr, tpr)
+... mean_tpr[0] = 0.0
+... roc_auc = auc(fpr, tpr)
+... plt.plot(fpr,
+... tpr,
+... label=f'ROC fold {i+1} (area = {roc_auc:.2f})')
+>>> plt.plot([0, 1],
+... [0, 1],
+... linestyle='--',
+... color=(0.6, 0.6, 0.6),
+... label='Random guessing (area=0.5)')
+>>> mean_tpr /= len(cv)
+>>> mean_tpr[-1] = 1.0
+>>> mean_auc = auc(mean_fpr, mean_tpr)
+>>> plt.plot(mean_fpr, mean_tpr, 'k--',
+... label=f'Mean ROC (area = {mean_auc:.2f})', lw=2)
+>>> plt.plot([0, 0, 1],
+... [0, 1, 1],
+... linestyle=':',
+... color='black',
+... label='Perfect performance (area=1.0)')
+>>> plt.xlim([-0.05, 1.05])
+>>> plt.ylim([-0.05, 1.05])
+>>> plt.xlabel('False positive rate')
+>>> plt.ylabel('True positive rate')
+>>> plt.legend(loc='lower right')
+>>> plt.show()
